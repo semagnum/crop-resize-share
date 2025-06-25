477c319333626457db96d2f86602d70c87b0c450
Spencer Magnusson <semagnum@gmail.com>
2025-06-25 07:55:50 -0600

    Initial site

---
+++ index.js
@@ -0,0 +1,127 @@
+const originalImage = document.getElementById('originalImage');
+const preview = document.getElementById('preview');
+const shareButton = document.getElementById('share');
+
+const errorMessage = document.getElementById('error');
+
+document.onpaste = function (event) {
+    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    for (var index in items) {
+        var item = items[index];
+        if (item.kind === 'file') {
+            const blob = item.getAsFile();
+            const url = URL.createObjectURL(blob);
+            updateImages(url);
+            break;
+        }
+    }
+};
+
+function updateImages(url) {
+    // reset values
+    document.getElementById('cropLeft').value = '';
+    document.getElementById('cropRight').value = '';
+    document.getElementById('cropTop').value = '';
+    document.getElementById('cropBottom').value = '';
+    document.getElementById('resizeWidth').value = '';
+    document.getElementById('resizeHeight').value = '';
+
+    originalImage.src = url;
+    originalImage.onload = () => {
+        updateCanvas();
+        document.getElementById("controls").style.display = 'block';
+        window.addEventListener("keypress", (e) => {
+            if (e.key === "Enter") {
+                updateCanvas();
+            }
+        });
+    };
+}
+
+function onImageUpload(e) {
+    const file = e.target.files[0];
+    if (file) {
+        url = URL.createObjectURL(file);
+        updateImages(url);
+    }
+}
+
+function updateCanvas() {
+    const cropLeft = parseInt(document.getElementById('cropLeft').value) || 0;
+    const cropRight = parseInt(document.getElementById('cropRight').value) || 0;
+    const cropTop = parseInt(document.getElementById('cropTop').value) || 0;
+    const cropBottom = parseInt(document.getElementById('cropBottom').value) || 0;
+
+    const newWidth = originalImage.width - cropLeft - cropRight;
+    const newHeight = originalImage.height - cropTop - cropBottom;
+
+    if (newWidth < 1 || newHeight < 1 || cropLeft < 0 || cropRight < 0 || cropTop < 0 || cropBottom < 0) {
+        errorMessage.style.display = 'block';
+        errorMessage.innerText = 'Invalid resize dimensions.';
+        return;
+    }
+    else {
+        errorMessage.style.display = 'none';
+    }
+
+    const canvas = document.createElement('canvas');
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(originalImage, cropLeft, cropTop, newWidth, newHeight, 0, 0, newWidth, newHeight);
+
+    if (errorMessage.style.display !== 'block') {
+        resize(canvas);
+    }
+    preview.src = canvas.toDataURL('image/png');
+    preview.style.display = 'inline';
+    document.getElementById('dimensions').innerText = canvas.width + "x" + canvas.height;
+}
+
+function resize(canvas) {
+    const resizeWidth = parseInt(document.getElementById('resizeWidth').value) || 0;
+    const resizeHeight = parseInt(document.getElementById('resizeHeight').value) || 0;
+
+    let width, height;
+
+    if (resizeWidth > 1 && resizeHeight > 1) {
+        width = resizeWidth;
+        height = resizeHeight;
+    }
+    else if (resizeWidth > 1) {
+        width = resizeWidth;
+        height = width * (canvas.height / canvas.width);
+    }
+    else if (resizeHeight > 1) {
+        height = resizeHeight;
+        width = height * (canvas.width / canvas.height);
+    }
+    else {  // no resize
+        return
+    }
+
+    current_width = canvas.width;
+    current_height = canvas.height;
+
+    while (current_width != width || current_height != height) {
+        if ((current_width / 2) > width && (current_height / 2) > height) {
+            current_width /= 2;
+            current_height /= 2;
+        }
+        else {
+            current_width = width;
+            current_height = height;
+        }
+
+        const oc = document.createElement('canvas');
+        oc.width = current_width;
+        oc.height = current_height;
+        oc.getContext('2d').drawImage(canvas, 0, 0, current_width, current_height);
+
+        canvas.width = current_width;
+        canvas.height = current_height;
+        canvas.getContext('2d').drawImage(oc, 0, 0, oc.width, oc.height, 0, 0, current_width, current_height);
+    }
+}
---
+++ sw_pages.js
@@ -0,0 +1,50 @@
+const cacheName = 'v1'; //Cache version name
+
+const cacheAssets = [ // list of files to cache
+    'index.html',
+    'index.css',
+    'index.js',
+
+    'assets/android-chrome-192x192.png',
+    'assets/android-chrome-512x512.png',
+    'favicon.ico'
+];
+
+
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log('Service Worker: Caching Files');
+                cache.addAll(cacheAssets);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+
+//Call install event
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    // Remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+//Call fetch Event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
